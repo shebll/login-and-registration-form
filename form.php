8d3c48bf0abd15e09d89c9ba9ac2a743b29f47b1
shebl <shebblloll@gmail.com>
2023-04-06 13:57:26 +0200

    initalize the pages structuer

---
+++ form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form</title>
+</head>
+<body>
+  <!-- start include the header file  -->
+  <?php 
+    include 'header.php';    
+  ?>  
+  <!-- End include the header file  -->
+  <!-- start the main form login   -->
+  
+  
+  <!-- start the main form login   -->
+  
+  <!-- start include the footer file  -->
+  <?php 
+    include 'footer.php';
+  ?>
+  <!-- end include the footer file  -->
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="headerStyle.css">
+  <title>Header</title>
+</head>
+<body>
+  <header>
+    <h2 class="logo">Logo</h2>
+    <div class="links">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><button class="btn-login">LOGIN</button></li>
+      </ul>
+    </div>
+  </header>
+</body>
+</html>
