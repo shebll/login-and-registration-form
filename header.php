a6a4ef16b053308353a1d95809279b7981779818
shebl <shebblloll@gmail.com>
2023-04-09 23:35:21 +0200

    close btn

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Contact</a></li>
-        <li><button class="btn-login">LOGIN</button></li>
+        <li><button class="btn-login">register</button></li>
       </ul>
     </div>
   </header>
