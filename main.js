11610f2cee7d3477d07b3050acff26609c5f6cb1
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2023-04-10 10:03:49 +0200

    integrating from master

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 let btn = document.querySelector(".btn-login");
+let close = document.querySelector(".close");
 let btnLoginLink = document.querySelector(".login-link");
 let btnRegisterLink = document.querySelector(".register-link");
 let loginForm = document.querySelector(".form-login");
 let registerForm = document.querySelector(".form-register");
 let wrapper = document.querySelector(".wrapper");
+
 btn.addEventListener("click", () => {
-  console.log(btn);
+  wrapper.style.transform = "translate(-50%,-70%) scale(1)";
+});
+
+close.addEventListener("click", () => {
+  wrapper.style.transform = "translate(-50%,-70%) scale(0)";
+  console.log("ss");
 });
 btnLoginLink.addEventListener("click", () => {
   // loginForm.style.display = "block";
@@ -18,5 +25,3 @@ btnRegisterLink.addEventListener("click", () => {
   wrapper.style.height = "740px";
   registerForm.style.right = "0%";
 });
-
-// console.log(btn);
